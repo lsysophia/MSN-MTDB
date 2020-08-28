18ff31609634980d41d169666ef0eab08e1c3134
Sophia Li <sophiali@Sophias-MBP.lan>
2020-08-28 15:54:59 -0500

    app intialized/setup middleware/setup localhost

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
+
+const app = express()
+require('dotenv').config()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+    session({
+        key: process.env.SECRET_KEY,
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: true,
+    }),
+)
+app.use(passport.initialize())
+app.use(passpo.session())
+
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
