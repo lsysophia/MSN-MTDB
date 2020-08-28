8453bc2d059684e1bc42ac20a4b24719924df595
nisozakuto <nisozakuto@gmail.com>
2020-08-28 17:52:45 -0400

    updated the db name

--- db/config.js
+++ db/config.js
@@ -9,7 +9,7 @@ const pgp = require('pg-promise')(options);
 function setDatabase() {
     if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
         return pgp({
-            database: 'msntv_dev',
+            database: 'msn_mtdb_dev',
             port = 5432,
             host: 'localhost',
         })
