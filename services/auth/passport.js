0f3f93724677d34da7dcc8ee8a2793ae3db6247c
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-08-28 20:20:50 -0600

    passport.js was created for real this time

--- services/auth/passport.js
+++ services/auth/passport.js
@@ -1,2 +1,16 @@
 const passport = require('passport');
-const User = require('../../models/User')
+const User = require('../../models/User');
+
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        console.log('serialize', user, '++++++++');
+        done(null, user.username);
+    });
+
+    passport.deserializeUser((username, done) => {
+        console.log('deserialize');
+        User.findByUsername(username)
+        .then(user => done(null, user))
+        .catch(err => done(err, null));
+    });
+};
