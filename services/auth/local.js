2a3f7e0a4990e18fba36621d19800b2c75b128c9
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-08-28 20:16:39 -0600

    fixed the capital U in Users model

--- services/auth/local.js
+++ services/auth/local.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 const init = require('./passport');
-const User = require('../../models/user');
+const User = require('../../models/User');
 const authHelpers = require('./auth-helpers');
 
 const options = {};
