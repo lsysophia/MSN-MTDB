c867b44331c0299e50495a506a3892469c7a68a9
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-08-29 21:06:43 -0600

    small variables added

--- db/migrations/20200829185500-create_user_movies_table.sql
+++ db/migrations/20200829185500-create_user_movies_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS user_movies (
     title VARCHAR(255) NOT NULL,
     imbd_id VARCHAR(255) NOT NULL,
     ratings INTEGER,
-    has_watched BOOLEAN,
+    has_watched BOOLEAN NOT NULL DEFAULT false,
     watched_time TIMESTAMP,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
