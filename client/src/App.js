e72749da9e9be17cfad147cdaecd55011d230aa8
nisozakuto <nisozakuto@gmail.com>
2020-08-29 22:59:52 -0400

    Header and Footer update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div >
+    <Router>
+      <div className="App">
+        <Header />
+      </div >
+    </Router>
   );
 }
 
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <footer>
+                Made by the best team of devs
+            </footer>
         )
     }
 }
