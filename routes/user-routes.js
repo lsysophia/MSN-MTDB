edc87891d5ec92724ebb364427f153ab59e1ab60
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-08-29 22:46:33 -0600

    users root route updated

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const userRouter = express.Router()
-const passport = require('../controllers/users-controller')
-const usersController = require('../controllers/users-controller')
 
-userRouter.get('/', usersController.index)
+const usersController = require('../controllers/users-controller')
+const moviesController = require('../controllers/movies-controller')
+const seriesController = require('../controllers/series-controller')
+const episodeController = require('../controllers/episodes-controller')
+
+userRouter.get('/', moviesController.index, seriesController.index, episodeController.index, usersController.index)
 
 userRouter.put('/edit/:id([0-9]+)', usersController.update)
 
