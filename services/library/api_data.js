ce769cc53cde51ce1727cba31f789194e75c550b
Sophia Li <sophiali@Sophias-MBP.lan>
2020-08-30 12:53:47 -0500

    initial fetch working

--- services/library/api_data.js
+++ services/library/api_data.js
@@ -1,29 +1,36 @@
 const fetch = require('node-fetch')
-const apiKey = process.env.API_Key
+require('dotenv').config()
 
-//initial fetch to get imdbid of the title
-const getImdbId = (title) => {
-	fetch(`https://imdb8.p.rapidapi.com/title/find?q=${title}`, {
-		// method: 'GET',
+const test = 'batman'
+// //initial fetch to get imdbid of the title
+const initialUnPack = (title) => {
+	return fetch(`https://imdb8.p.rapidapi.com/title/find?q=${title}`, {
+		method: 'GET',
 		headers: {
-			// host: 'imdb8.p.rapidapi.com',
-			key: process.env.API_Key
+			'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+			'x-rapidapi-key': process.env.API_Key
 		},
 	})
-	.then((res) => {
-		// console.log(res.json())
-		return res.json()
+	.then(res => 
+		res.json()
+	)
+	.then(parsedRes => {
+		return parsedRes.results.map((obj) => {
+			if(obj.titleType) {
+				const result = {
+					id: obj.id,
+					title: obj.title,
+					image: obj.image.url
+				}
+				console.log(result)
+				return result
+			}
+		})
 	})
-	.then((parsedRes) => {
-		console.log('parsed' + parsedRes)
-		// const imdbId = parsedRes.results[0].id
-		// console.log(imdbId)
-	})
-	.catch(err => {
-		console.log(err);
-	});
+	.catch(err => 
+		console.log(err)
+	);
 
 }
 
-const getBatmanID = getImdbId('Batman')
-console.log(`this is Batman:  ${getBatmanID}`)
+console.log(initialUnPack(test))
