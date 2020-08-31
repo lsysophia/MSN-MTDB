09d7fb863491dc8264359996eae6a4800e49a965
nisozakuto <nisozakuto@gmail.com>
2020-08-31 10:42:49 -0400

    can be deleted stash

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header'
 import User from './components/User'
 import Home from './components/Home'
+import Search from './components/Search'
+import Show from './components/Show'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -12,9 +14,7 @@ function App() {
       <div className="App">
         <Header />
         <div className="container">
-          <User />
-
-          <Home />
+          <Show />
         </div>
 
       </div >
