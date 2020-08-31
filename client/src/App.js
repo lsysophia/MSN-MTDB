c922fca23f3c69543b75e4446636146b30dfa1dc
Sophia Li <sophiali@Sophias-MBP.lan>
2020-08-31 15:12:48 -0500

    client side user auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header'
 import User from './components/User'
 import Home from './components/Home'
+import Footer from './components/Footer'
+import Login from './components/Login'
+import Register from './components/Register'
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import fetch from 'node-fetch';
+import { json } from 'body-parser';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="container">
-          <User />
-          <Home />
-        </div>
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      auth: false,
+      user: null,
+    }
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this)
+    this.handleRegisterSubmit = this.handleRegisterSubmit.bind(this)
 
-      </div >
-    </Router>
-  );
+  }
+
+  componentDidMount() {
+    fetch('/api/auth/verify', { credentials: 'include' })
+      .then(res => res.json())
+      .then(parsedRes => {
+        this.setState({
+          auth: parsedRes.auth,
+          user: parsedRes.data.user,
+        })
+      }).catch(err => console.log(err))
+  }
+
+  handleLoginSubmit(e, data) {
+    e.preventDefault()
+    fetch('/api/auth/login', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(data), 
+    }).then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes)
+        this.setState({
+          auth: parsedRes.auth,
+          user: parsedRes.data.user,
+        })
+      }).catch(err => console.log(err))
+  }
+
+  handleRegisterSubmit(e, data) {
+    e.preventDefault()
+    console.log(data)
+    fetch('/api/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(data),
+    }).then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes)
+        this.setState({
+          auth: parsedRes.auth,
+          user: parsedRes.data.user,
+        })
+      }).catch(err => console.log(err))
+  }
+
+  logout() {
+    fetch('/api/auth/logout', {
+      credentials: 'include',
+    }).then(res => res.json())
+      .then(parsedRes => {
+        this.setState({
+          auth: parsedRes.auth,
+          user: parsedRes.data.user
+        })
+      }).catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login'
+              render={() => (
+                this.state.auth
+                ? <Redirect to='/user' />
+                : <Login handleLoginSubmit={this.handleLoginSubmit} />
+              )}
+            />
+            <Route exact path='/register'
+              render={() => (
+                this.state.auth
+                ? <Redirect to='user' />
+                : <Register handleRegisterSubmit={this.handleRegisterSubmit} />
+              )}
+            />
+            <Route exact path='/user'
+              render={() => (
+                !this.state.auth
+                ? <Redirect to='/login' />
+                : <User user={this.state.user} />
+              )}
+            />
+            {/* Add conditional so that logout would only show up when 
+            this.state.auth is checked out? */}
+          </div>
+          <Footer />
+        </div >
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
+    }
+
+    handleInputChange(e) {
+        const name = e.currentTarget.name
+        const value = e.currentTarget.value
+        this.setState({
+            [name]: value,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
+                    <input type="text"
+                    name="username"
+                    value={this.state.username}
+                    placeholder="Username"
+                    onChange={this.handleInputChange}
+                    />
+                    <input type="password"
+                    name="password"
+                    value={this.state.password}
+                    placeholder="Password"
+                    onChange={this.handleInputChange}
+                    />
+                    <input type="email"
+                    name="email"
+                    value={this.state.email}
+                    placeholder="Email"
+                    onChange={this.handleInputChange}
+                    />
+                    <input type="submit"
+                    value="Register"
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Register
