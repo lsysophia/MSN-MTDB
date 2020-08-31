10d8ea410de00565a26c79e3038a87d07e925d44
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-08-30 22:14:35 -0600

    fixed typo

--- controllers/user_movies-controller.js
+++ controllers/user_movies-controller.js
@@ -3,7 +3,7 @@ const User_movies = require('../models/User_movies')
 const user_moviesController = {}
 
 user_moviesController.index = (req, res, next) => {
-    User_movies.getAllForUser()
+    User_movies.getAllForUser(req.user.id)
     .then(movies => {
         res.locals.movies = movies
         next();
