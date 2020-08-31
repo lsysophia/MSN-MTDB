6e02e6e5847677638089e3d7f4979b28bdced1b2
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-08-30 21:09:51 -0600

    building small searchController

--- routes/search-routes.js
+++ routes/search-routes.js
@@ -4,6 +4,7 @@ const searchRouter = express.Router()
 const user_moviesController = require('../controllers/user_movies-controller')
 const user_seriesController = require('../controllers/user_series-controller')
 const searchController = require('../controllers/searchController')
+// const { searchFind, searchDetails } = require('../services/find')
 
 searchRouter.get('/', user_moviesController.index, user_seriesController.index, (req, res) => {
     res.json({
@@ -15,6 +16,6 @@ searchRouter.get('/', user_moviesController.index, user_seriesController.index, 
     })
 })
 
-searchRouter.post('/:id([0-9]+)', )
+searchRouter.post('/:id([0-9]+)',  searchController.decide) // will need the search fetches
 
 module.exports = searchRouter
