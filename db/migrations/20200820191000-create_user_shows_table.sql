9c484b68b724fb6c8dfb147ed54146b285fb7a23
Sophia Li <sophiali@Sophias-MBP.lan>
2020-08-31 16:36:12 -0500

    register form updated

--- db/migrations/20200820191000-create_user_shows_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_shows (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    imdb_id VARCHAR(255) NOT NULL,
-    ratings INTEGER,
-    has_watched BOOLEAN NOT NULL DEFAULT false,
-    watched_time TIMESTAMP,
-    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
-);
