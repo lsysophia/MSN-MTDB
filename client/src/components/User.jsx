24e820181cdeb609d68a28c47fd54b163f17248f
nisozakuto <nisozakuto@gmail.com>
2020-09-01 16:42:46 -0400

    styling edits

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -4,24 +4,25 @@ import { Link } from 'react-router-dom'
 export default class User extends Component {
     render() {
         return (
-            <div className="user-page">
-                <section>
-                    <h2>Username</h2>
-                    <div className="user-settings">
-                        <p>Log out</p>
-                        <p><Link to='/user/edit'>Edit Info</Link></p>
-                        <p onClick={() => {this.props.deleteUser(this.props.user.id)}}>Delete Account</p>
+            <div>
+                <div className="user-page">
+                    <section>
+                        <h2>Username</h2>
+                    </section>
+                    <aside>
+                        <h2>User's saved movie list</h2>
+                        <div className="user-saved-list">
+                            List comes here
                     </div>
-                </section>
-
-                <aside>
-                    <h2>User's saved movie list</h2>
-                    <div>
-                        List comes here
-                    </div>
-                </aside>
-
+                    </aside>
+                </div>
+                <div className="user-settings">
+                    <h4>Log out</h4>
+                    <h4><Link to='/user/edit'>Edit Info</Link></h4>
+                    <h4 onClick={() => { this.props.deleteUser(this.props.user.id) }}>Delete Account</h4>
+                </div>
             </div>
+
         )
     }
 }
