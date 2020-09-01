eec0f931c6afcb83473aca6e4bbc0a5bd08f1f12
nisozakuto <nisozakuto@gmail.com>
2020-08-31 21:55:52 -0400

    login and register css

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -21,7 +21,7 @@ export default class Login extends Component {
     render() {
         return (
             <div>
-                <form onSubmit={(e) => this.props.handleLoginSubmit(e, this.state)} >
+                <form className="form-box" onSubmit={(e) => this.props.handleLoginSubmit(e, this.state)} >
                     <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={this.handleInputChange} />
                     <input type="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
                     <input type="submit" value="Log in!" />
