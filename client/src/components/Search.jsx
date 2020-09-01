bfdaa8d37c534043abe59b55fb19ed915a839750
Matthew Schlaff <schmayley@Matthews-MacBook-Air.local>
2020-09-01 08:42:35 -0600

    built constructor in search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+        }
+    }
     render() {
         return (
             <div className="search-page">
