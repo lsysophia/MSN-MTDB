1f6a95fd531143bd5699489a68de9f919d38477c
nisozakuto <nisozakuto@gmail.com>
2020-08-31 22:44:59 -0400

    About page update

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import Register from './components/Register'
+import About from './components/About'
 import Search from './components/Search'
 
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
@@ -116,6 +117,13 @@ class App extends Component {
             <Route exact path='/search'
               render={() => <Search search={this.state.user} />}
             />
+
+
+            <Route exact path='/about'
+              render={() => <About search={this.state.user} />}
+            />
+
+
           </div>
           <Footer />
         </div >
