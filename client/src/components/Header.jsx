5b11b7f40cc54575fd35c8899335395ad5d64bfe
Sophia Li <sophiali@Sophias-MBP.lan>
2020-09-01 15:05:53 -0500

    user logout login switching display

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,12 +11,16 @@ export default class Header extends Component {
                 <nav>
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/user">Profile</Link></li>
                         <li><Link to="/search">Search</Link></li>
                         <li><Link to="/about">About</Link></li>
-                        <li className="logout-click" onClick={() => { this.props.logout() }}>Logout</li>
+                        {this.props.userAuth
+                        ?
+                        <ul> 
+                            <li><Link to="/user">Profile</Link></li> 
+                            <li className="logout-click" onClick={() => { this.props.logout() }}>Logout</li>
+                        </ul>
+                        : <li><Link to='/login'>Login</Link></li>}
                     </ul>
-                    {/* Will use ternary here when we have Auth function working*/}
                 </nav>
             </header>
         )
