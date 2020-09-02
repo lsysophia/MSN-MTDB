78cee38761b1745034da49e8126565bce30781d9
nisozakuto <nisozakuto@gmail.com>
2020-09-01 21:16:53 -0400

    further changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,14 +9,14 @@ export default class Home extends Component {
         return (
             <div className="Home-page">
                 <div>
-                    <h1>Welcome to MSNMTVDB</h1>
+                    <h1>Welcome to MSN - MTVDB</h1>
                 </div>
                 <div className="user-box">
                     <h3><Link to='/login'>Login</Link></h3>
                     <h3><Link to='/register'>Register</Link></h3>
                 </div>
-                <div>
-                    <h3>Search Page</h3>
+                <div className="user-box">
+                    <h3><Link to="/search">Search Page</Link></h3>
                 </div>
             </div>
         )
