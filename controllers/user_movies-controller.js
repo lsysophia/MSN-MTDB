6bd95c5e661bc94bf315d4ff944c5c5033359dad
nisozakuto <nisozakuto@gmail.com>
2020-09-02 10:58:35 -0400

    user_moviescontroller create func

--- controllers/user_movies-controller.js
+++ controllers/user_movies-controller.js
@@ -4,11 +4,25 @@ const user_moviesController = {}
 
 user_moviesController.index = (req, res, next) => {
     User_movies.getAllForUser(req.user.id)
-    .then(movies => {
-        res.locals.movies = movies
-        next();
-    })
-    .catch(next)
+        .then(movies => {
+            res.locals.movies = movies
+            next();
+        })
+        .catch(next)
+}
+
+user_moviesController.create = (req, res, next) => {
+    new Movie({
+        title: req.body.title,
+        imdb_id: req.body.imdb_id,
+        user_id: req.body.user_id,
+    }).save()
+        .then(movie => {
+            res.json({
+                message: 'Movi added to users saved list',
+                data: { movie }
+            })
+        }).catch(next);
 }
 
 module.exports = user_moviesController
