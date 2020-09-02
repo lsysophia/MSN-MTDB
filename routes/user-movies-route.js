34aab829ccf48251e9ab404938ef582fa8775143
nisozakuto <nisozakuto@gmail.com>
2020-09-02 11:24:55 -0400

    user-movies-route index and create

---
+++ routes/user-movies-route.js
@@ -0,0 +1,11 @@
+const userMoviesRoute = require('express').Router()
+
+const userMoviesController = require('../controllers/user_movies-controller')
+
+userMoviesRoute.get('/', userMoviesController.index)
+userMoviesRoute.post('/', userMoviesController.create)
+// userMoviesRoute.get('/', userMoviesController.show)
+// userMoviesRoute.put('/', userMoviesController.update)
+// userMoviesRoute.delete('/', userMoviesController.delete)
+
+module.exports = userMoviesRoute
