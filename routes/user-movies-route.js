be406fcd1658d7bb0746eb0fd2455aa33362f5a9
nisozakuto <nisozakuto@gmail.com>
2020-09-02 14:30:26 -0400

    movies route put and delete

--- routes/user-movies-route.js
+++ routes/user-movies-route.js
@@ -5,7 +5,7 @@ const userMoviesController = require('../controllers/user_movies-controller')
 userMoviesRoute.get('/', userMoviesController.index)
 userMoviesRoute.post('/', userMoviesController.create)
 // userMoviesRoute.get('/', userMoviesController.show)
-// userMoviesRoute.put('/', userMoviesController.update)
-// userMoviesRoute.delete('/', userMoviesController.delete)
+userMoviesRoute.put('/', userMoviesController.update)
+userMoviesRoute.delete('/', userMoviesController.delete)
 
 module.exports = userMoviesRoute
