f499fff902d2e238b141c475f040023d06d2dc27
nisozakuto <nisozakuto@gmail.com>
2020-09-02 13:05:55 -0400

    user_movies model update

--- models/User_movies.js
+++ models/User_movies.js
@@ -13,9 +13,9 @@ class User_movies {
 
     static getAll() {
         return db
-        .manyOrNone('SELECT * FROM user_movies ORDER BY title ASC')
-        .then(movies => movies.map(movie => new this(movie)))
-        .catch(err => console.log(err))
+            .manyOrNone('SELECT * FROM user_movies ORDER BY title ASC')
+            .then(movies => movies.map(movie => new this(movie)))
+            .catch(err => console.log(err))
     }
 
     static getAllForUser(user_id) {
@@ -24,36 +24,54 @@ class User_movies {
             JOIN user_movies ON users.id = user_movies.user_id
             WHERE users.id = $1`, user_id
         )
-        .then(movies => movies.map(movie => new this(movie)))
-        .catch(err => console.log(err))
+            .then(movies => movies.map(movie => new this(movie)))
+            .catch(err => console.log(err))
     }
 
     static getById(id) {
         return db
-        .oneOrNone('SELECT * FROM user_movies WHERE id = $1', id)
-        .then(movie => new this(movie))
-        .catch(err => console.log(err))
+            .oneOrNone('SELECT * FROM user_movies WHERE id = $1', id)
+            .then(movie => new this(movie))
+            .catch(err => console.log(err))
     }
 
     static getOneForUser(user_id, userMovie_id) {
         return db
-        .manyOrNone('SELECT * FROM user_movies WHERE user_id = $1 AND id = $2', [user_id, userMovie_id])
-        .then(movie => new this(movie[0]))
-        .catch(err => console.log(err))
+            .manyOrNone('SELECT * FROM user_movies WHERE user_id = $1 AND id = $2', [user_id, userMovie_id])
+            .then(movie => new this(movie[0]))
+            .catch(err => console.log(err))
     }
 
     save() {
         return db
-        .one(
-            `INSERT INTO user_movies
+            .one(
+                `INSERT INTO user_movies
             (title, imdb_id, ratings, has_watched, watched_time, user_id)
             VALUES
             ($/title/, $/imdb_id/, $/ratings/, $/has_watched/, $/watched_time/, $/user_id/)
             RETURNING *`,
-            this
-        )
-        .then(savedMovie => Object.assign(this, savedMovie))
-        .catch(err => console.log(err))
+                this
+            )
+            .then(savedMovie => Object.assign(this, savedMovie))
+            .catch(err => console.log(err))
+    }
+
+    update(changes) {
+        Object.assign(this, changes)
+        return db
+            .one(
+                `
+            UPDATE user_movies SET
+            title = $/title/, 
+            imdb_id = $/imdb_id/,
+            ratings = $/ratings/,
+            has_watched = $/has_watched/,
+            watched_time = $/watched_time/,
+            user_id = $/user_id/
+            RETURNING *`,
+                this
+            )
+            .then((movie) => Object.assign(this, movie));
     }
 
     delete() {
