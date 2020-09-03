4080c897687aa8ce08d1038bca85a1b6d7ca769f
Sophia Li <sophiali@Sophias-MBP.lan>
2020-09-02 21:03:32 -0500

    kind of stuck on render watchlist

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -7,7 +7,7 @@ export default class User extends Component {
         this.setState={
             watchlist: null, 
         }
-        this.chooseToRender = this.chooseToRender.bind(this)
+        // this.chooseToRender = this.chooseToRender.bind(this)
     }
 
     componentDidMount() {
@@ -27,14 +27,24 @@ export default class User extends Component {
         }).catch(err => console.log(err))
     }
 
-    chooseToRender() {
-        if (this.state.watchlist) {
-            this.state.watchlist.map(each => {
-                console.log(each)
-                return 
-            })
-        }
-    }
+    // chooseToRender() {
+    //     if (this.state.watchlist.userEpisodes.length > 0) {
+    //         this.state.watchlist.userEpisodes.map(eachEpisode => {
+    //             console.log(eachEpisode)
+    //             return eachEpisode.title
+    //         })
+    //     } if (this.state.watchlist.userMovies.length > 0) {
+    //         this.state.watchlist.userMovies.map(eachMovie => {
+    //             return eachMovie.title
+    //         })
+    //     } if (this.state.watchlist.userSeries.length > 0) {
+    //         this.state.watchlist.userSeries.map(eachSeries => {
+    //             return eachSeries.title
+    //         })
+    //     } else {
+
+    //     }
+    // }
 
     render() {
         return (
@@ -42,12 +52,34 @@ export default class User extends Component {
                 <div className="user-page">
                     <section>
                         <h2>{this.props.user.username}</h2>
+                        <div className="user-info">
+                            <p>Name: {this.props.user.name}</p>
+                            <p>Email: {this.props.user.email}</p>
+                            <p>Age: {this.props.user.age}</p>
+                            <p>Preferred Genres: {this.props.user.genres}</p>
+                        </div>
                     </section>
                     <aside>
-                        <h2>User's saved movie list</h2>
+                        <h2>User's saved watchlist</h2>
                         <div className="user-saved-list">
-                          
-                            
+                            <ul>
+                                { (!this.state.watchlist) ?
+                                    <h3>Your list is empty. Go add something!</h3>
+                                    : (this.state.watchlist.userEpisodes.length > 0) ?
+                                    this.state.watchlist.userEpisodes.map(eachEpisode => {
+                                        return <li key={eachEpisode.id}>{eachEpisode.title}</li>
+                                    })
+                                    : (this.state.watchlist.userMovies.length > 0) ?
+                                    this.state.watchlist.userMovies.map(eachMovie => {
+                                        return <li key={eachMovie.id}>{eachMovie.title}</li>
+                                    })
+                                    : (this.state.watchlist.userSeries.length > 0) ?
+                                    this.state.watchlist.userSeries.map(eachSeries => {
+                                        return <li key={eachSeries.id}>{eachSeries.title}</li>
+                                    })
+                                    : null
+                                }
+                            </ul>
                         </div>
                     </aside>
                 </div>
